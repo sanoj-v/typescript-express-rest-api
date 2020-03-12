2fdbbaec5e41e6ff88ac6485b2cabbea2e960df0
Sanoj_V <39582192+sanoj-v@users.noreply.github.com>
2020-03-12 15:45:37 +0530

    Update model.ts

--- src/components/User/model.ts
+++ src/components/User/model.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import * as connections from '@/config/connection/connection';
 import * as crypto from 'crypto';
 import { Document, Schema } from 'mongoose';
